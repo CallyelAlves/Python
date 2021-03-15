5e096a20e952b12fd39fdd9495a5af886cbb69af
Callyel Alves <callyelalves@hotmail.com>
2021-03-15 20:36:57 -0300

    Update Matriz com times

--- Algoritmos com Python/Matriz com times.py
+++ Algoritmos com Python/Matriz com times.py
@@ -5,3 +5,57 @@ O usuário deve, inicialmente, fornecer os nomes de todos os times que se enfren
 
 """
 
+#criação de variáveis
+empate = 0
+linhas = 10
+colunas = 2
+times = []
+placar = []
+vencedores = []
+
+#criando as matrizes
+for i in range(linhas):
+    times.append([0] * (colunas))
+
+for i in range(linhas):
+    placar.append([0] * (colunas))
+
+#add valores as matrizes
+for i in range(linhas):
+  print('Patida', i+1)
+  for a in range(colunas):
+   print('Digite o time', a+1)
+   times[i][a] = input()
+
+print()
+
+for i in range(linhas):
+  for a in range(colunas):
+    print('Digite quantos gols fez', times[i][a])
+    placar[i][a] = int(input())
+
+#ediçao de saída da matriz
+for i in range(linhas):
+  for a in range(colunas):
+    print(times[i][a],' ', end='')
+  print()
+
+print()
+for i in range(linhas):
+  for a in range(colunas):
+    print(placar[i][a],' ', end='')
+  print()
+print()
+
+#condições das matrizes
+for i in range(linhas):
+  if placar[i][0] > placar[i][1]:
+    vencedores.append(times[i][0])
+  elif placar[i][0] < placar[i][1]:
+    vencedores.append(times[i][1])
+  else:
+    empate +=1
+if empate == linhas:
+  print('Todos os jogos foi empate')
+else:
+  print('Times que venceram seus jogos', *vencedores, sep=' - ')
